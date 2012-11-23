123648129a832677572537c480b3216b0858479b
Stevan Clement <sclement@ualberta.ca>
2012-11-22 21:23:14 -0700

    Just some changes.

--- watch.py
+++ watch.py
@@ -7,92 +7,106 @@ import re
 import base64
 import sys
 import json
-
-"""
-Important Javascript:
-
-This returns an array of numbers.
-function HtB(s)
-{
-	var b=[];
-
-	for(var i=0;i<s.toString().length;i+=2) {
-		b[i/2]=(0x00+parseInt(s.substr(i,2),16))&(255);
-	}
-
-	return b;
-}
-
-This JavaScript function takes an array of numbers (in the range 0-255) and converts to a base64-encoded string,
-then breaks long lines if necessary:
-function e64(data)
-{
-  var str = "";
-  for (var i = 0; i < data.length; i++)
-    str += String.fromCharCode(data[i]);
-
-  return btoa(str).split(/(.{75})/).join("\n").replace(/\n+/g, "\n").trim();
-}
-"""
+from operator import itemgetter
 
 def htb(data):
-	"""
-	Converts string of hex into list of decimals.
-	"""
-	i = 0
-	b = []
-	while i < len(data):
-		b.append(0x00 + int(data[i:i+2], 16) & (255))
-		i = i + 2
-	return b
+    """
+    Converts string of hex into list of decimals.
+    """
+    i = 0
+    b = []
+    while i < len(data):
+        b.append(0x00 + int(data[i:i+2], 16) & (255))
+        i = i + 2
+    return b
 
 def encode64(data):
-	"""
-	Converts list of integers [0..255] to a string.
-	"""
-	encodedString = ""
-	for num in data:
-		encodedString += str(chr(num))
-
-	encodedString = base64.b64encode(encodedString)
-	return encodedString
+    """
+    Converts list of integers [0..255] to a string.
+    """
+    encodedString = ""
+    for num in data:
+        encodedString += str(chr(num))
+        
+    encodedString = base64.b64encode(encodedString)
+    return encodedString
 
 def getAuctionInfo(auction):
-	encodedString = encode64(htb(auction))
-	request = "auct="+ urllib.quote(encodedString.encode("utf-8")) +"&lts=0&t=0"
+    encodedString = encode64(htb(auction))
+    request = "auct="+ urllib.quote(encodedString.encode("utf-8")) +"&lts=0&t=0"
 
-	site = urllib.urlopen("http://quibidsinsider.com/ajax/", request)
+    site = urllib.urlopen("http://quibidsinsider.com/ajax/", request)
 
-	content = site.read() 
-	site.close()
-	return content
+    content = site.read()
+    site.close()
+    return content
 
 def getCurrentAuctions():
-	"""
-	http://stackoverflow.com/a/1732454
-	"""
-	site = urllib.urlopen("http://quibidsinsider.com/auction/?page=1")
-	siteContent = site.read()
-	site.close()
-	oddAuctions = [m.start() for m in re.finditer('row odd auction-', siteContent)]
-	evenAuctions = [m.start() for m in re.finditer('row even auction-', siteContent)]
-	auctions = [siteContent[i+16:i+32] for i in oddAuctions] + [siteContent[i+17:i+33] for i in evenAuctions]
+    """
+    http://stackoverflow.com/a/1732454
+    """
+    site = urllib.urlopen("http://quibidsinsider.com/auction/?page=1")
+    siteContent = site.read()
+    site.close()
+    oddAuctions = [m.start() for m in re.finditer('row odd auction-', siteContent)]
+    evenAuctions = [m.start() for m in re.finditer('row even auction-', siteContent)]
+    auctions = [siteContent[i+16:i+32] for i in oddAuctions] + [siteContent[i+17:i+33] for i in evenAuctions]
 
-	# You can actually send all the ids at once
-	id = ''.join(n for n in auctions)
-	info =  getAuctionInfo(id)
-	return info
+    auctionInfo = []
+    auctionDict = {}
+
+    i = 0
+    for a in auctions:
+        auctionDict = {}
+        auctionDict['id'] = a
+        reTitle = re.compile('/auction/' + auctions[i] + '/"(?:(?!</a>).)*')
+        auctionDict['name'] = reTitle.search(siteContent).group(0)[28:]
+        auctionInfo.append(auctionDict)
+        i = i + 1
+
+    return auctionInfo
 
 def prettyPrint():
-	data = json.loads(getCurrentAuctions())
+    auctionInfo = getCurrentAuctions()
 
-	for i in range(len(data["a"])):
-		# Grab the dictionary for a specific auction, append the auction number
-		auctionInfo = data["a"][i][data["a"][i].keys()[0]]
-		auctionInfo['auctionID'] = data["a"][i].keys()[0]
-		print auctionInfo
-	
-	
+    print auctionInfo
+
+    while True:
+        time.sleep(1)
+    
+        # You can actually send all the ids at once
+        idString = ''.join(n['id'] for n in auctionInfo)
+    
+        info = getAuctionInfo(idString)
+        data = json.loads(info)
+        lastAuctionUpdate = data['lts']
+        print "Last Auction Update : " + str(lastAuctionUpdate)
+        print "Number of auctions being watched : " + str(len(auctionInfo))
+        for i in range(len(data["a"])):
+            # Grab the dictionary for a specific auction, append the auction number
+            auctionJSON = data["a"][i][data["a"][i].keys()[0]]
+            auctionJSON['auctionID'] = data["a"][i].keys()[0]
+
+            try:
+                print auctionInfo[map(itemgetter('id'), auctionInfo).index(str(data["a"][i].keys()[0]).upper())]['name'] + \
+                    "|$|" + str(auctionJSON['auctionID']) + \
+                    "|$|" + str(auctionJSON['p']) + \
+                    "|$|" + str(auctionJSON['w']) + \
+                    "|$|" + str(auctionJSON['tl']) + \
+                    "|$|" + str(lastAuctionUpdate)
+            except KeyError:
+                print auctionInfo[map(itemgetter('id'), auctionInfo).index(str(data["a"][i].keys()[0]).upper())]['name'] + \
+                    "|$|" + str(auctionJSON['auctionID']) + \
+                    "|$|" + str(auctionJSON['p']) + \
+                    "|$|" + str(auctionJSON['w']) + \
+                    "|$|" + "0" \
+                    "|$|" + str(auctionJSON['e'])
+                # Auction must be over, remove it from the list
+                auctionInfo.pop(map(itemgetter('id'), auctionInfo).index(str(data["a"][i].keys()[0]).upper()))
+
+        if not auctionInfo:
+            # All of the auctions we are monitoring have completed
+            break
 
 if __name__ == "__main__":
-	prettyPrint()
+    prettyPrint()
