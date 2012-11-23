3df691be7e5a974f92930ed02016657aa6a216f6
Stevan Clement <sclement@ualberta.ca>
2012-11-22 22:17:41 -0700

    eXtreme programming in session

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import subprocess
+import watch
 
 # import server
 # s = server.Server()
@@ -17,12 +18,25 @@ class Server:
 		self.max =22
 
 
-	def launchProgram(self,prog = "hello.py",args=[],file_name = "test1.txt"):
-		for arg in args:
-			arg_string = arg_string  + " " + arg
-		prog = prog + arg_string
-		subprocess.call(["ssh",self.hosts[self.counter], "python ~/466/project/Auctioneer/"+ prog +" > ~/466/project/data/" + file_name]);
+	def launchProgram(self,prog = "client.py",args=[],file_name = "test1.txt"):
+#		prog = prog+args
+                call1 = ["python"]
+                call1.append("~/C466/Project/Auctioneer/" + prog) # +" > ~/C466/Project/Data/" + file_name
+                call1.append(args)
+                print call1
+                subprocess.call(call1)
+#		subprocess.call(["ssh",self.hosts[self.counter], "python ~/466/project/Auctioneer/"+ prog +" > ~/466/project/data/" + file_name]);
 		self.counter = (self.counter +1)% self.max
 
+if __name__ == "__main__":
+        s = Server()
 
+        siteContent = watch.getSiteContent(1)
+        auctionList = watch.getCurrentAuctions(siteContent)
+        auctionInfo = watch.compileCurrentAuctions(auctionList, siteContent)
 
+        arg = ""
+        for a in auctionInfo:
+                arg = arg + str(a['id']) + ' "' + str(a['name']) + '" '
+
+        s.launchProgram(args=arg)
