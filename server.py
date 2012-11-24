d719cd2d1b32cc0ebc4eaf648a3c5fad82a7b203
Stevan Clement <sclement@ualberta.ca>
2012-11-23 17:24:24 -0700

    Added timer to server. Checks every 5 minutes and starts to processes.

--- server.py
+++ server.py
@@ -1,38 +1,61 @@
 import subprocess
 import watch
-import 
+import time
+import os
+from operator import itemgetter
 # import server
 # s = server.Server()
 # s.launchProgram(prog = "name of program",args=[cmd line args],file_name = "to be redirected to")
 
 class Server:
 
-	def __init__(self,user = "sar7"):
+	def __init__(self):
 		self.hosts = []
 		for i in range(1,23):
 			if i<10:
-				self.hosts.append(user+"@uf0"+str(i)+".cs.ualberta.ca")
+				self.hosts.append(os.getenv('USER')+"@uf0"+str(i)+".cs.ualberta.ca")
 			else:
-				self.hosts.append(user+"@uf"+str(i)+".cs.ualberta.ca")
+				self.hosts.append(os.getenv('USER')+"@uf"+str(i)+".cs.ualberta.ca")
 		self.counter = 0
 		self.max =22
 
 
 	def launchProgram(self,prog = "client.py",args=[],file_name = "test1.txt"):
 		prog = prog+" " +args
-		subprocess.call(["ssh",self.hosts[self.counter], "python ~/466/project/Auctioneer/"+ prog +" > ~/466/project/data/" + file_name]);
+		subprocess.call(["ssh",self.hosts[self.counter], "python " + os.getenv('REPO_PATH') + prog +" > " + os.getenv('DB_PATH') + file_name])
 		self.counter = (self.counter +1)% self.max
 
 if __name__ == "__main__":
         s = Server()
 
-        siteContent = watch.getSiteContent(1)
-        auctionList = watch.getCurrentAuctions(siteContent)
-        auctionInfo = watch.compileCurrentAuctions(auctionList, siteContent)
+        pageNum = 4
+        auctionInterval = 60*5
+        auctionInfo = []
 
-        arg = ""
-        for a in auctionInfo:
-                arg = arg + str(a['id']) + ' "' + str(a['name']) + '" '
-		
-	f = str(time.time())+".txt"
-        s.launchProgram(args=arg,file_name=f)
+        while True:
+                tempAuctionInfo = []
+                siteContent = watch.getSiteContent(pageNum)
+                auctionList = watch.getCurrentAuctions(siteContent)
+                tempAuctionInfo = watch.compileCurrentAuctions(auctionList, siteContent)
+
+                # Just check against last auctions to see if we have any of the same ones
+                for tempAuc in tempAuctionInfo:
+                        for auc in auctionInfo:
+                                if auc['id'] == tempAuc['id']:
+                                        tempAuctionInfo.pop(map(itemgetter('id'), tempAuctionInfo).index(str(tempAuc['id']).upper()))
+
+                auctionInfo = tempAuctionInfo
+
+                arg = ""
+                for a in auctionInfo:
+                        arg = arg + str(a['id']) + ' "' + str(a['name']) + '" '
+                        
+                f = str(time.time())+".txt"
+                print "Auction Request - Starting New Process"
+                print "Number of auctions : " + str(len(auctionInfo))
+                print arg
+                print "###################"
+                s.launchProgram(args=arg,file_name=f)
+
+                time.sleep(auctionInterval)
+                
