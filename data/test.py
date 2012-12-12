9f81933bb052a5359af6fd2b67982d1647591f08
Stephen Rebstock <sar7@cs.ualberta.ca>
2012-12-11 20:12:39 -0700

    asdfasd

--- data/test.py
+++ data/test.py
@@ -19,22 +19,21 @@ def printStats(y,yhat,algorithm,weight,beta,label):
                 predictNotEnd[0] = predictNotEnd[0] + 1
     if (float(predictEnd[0])/float(predictEnd[1]))*(float(predictNotEnd[0])/float(predictNotEnd[1])) != 0:
         print label
-        print algorithm + "  " + str(weight) + "  " + str(beta)
-        print "Accuracy on winning bids = "+str(float(predictEnd[0])/float(predictEnd[1]))+ "  " + str(predictEnd[0])+"//"+str(predictEnd[1])
-        print "Accuracy on non-winning bids = "+str(float(predictNotEnd[0])/float(predictNotEnd[1])) + str(predictNotEnd[0])+"//"+str(predictNotEnd[1])
+        print algorithm + "  " + str(weight) + "  " + str(beta) + " "
+        print "Accuracy on winning bids = "+str(float(predictEnd[0])/float(predictEnd[1]))+ "  " + str(predictEnd[0])+"/"+str(predictEnd[1])
+        print "Accuracy on non-winning bids = "+str(float(predictNotEnd[0])/float(predictNotEnd[1]))  + "  "+ str(predictNotEnd[0])+"/"+str(predictNotEnd[1])
 
 def shuffle_in_unison_inplace(a, b):
     assert len(a) == len(b)
     p = np.random.permutation(len(a))
     return a[p], b[p]
 
-
-def main(xfile,yfile):
+def main(xfile,yfile,algorithm=""):
     x = np.loadtxt(open(xfile,"rb"),delimiter=" ")
     y = np.loadtxt(open(yfile,"rb"),delimiter=",")
 
     
-    x , y = shuffle_in_unison_inplace(x,y)
+    x,y = shuffle_in_unison_inplace(x,y)
 
     tr_size = 1000
     te_size = 1000
@@ -42,25 +41,49 @@ def main(xfile,yfile):
     xtrain = x[0:tr_size]
     xtest = x[tr_size:(tr_size+te_size)]
 
-    y = np.loadtxt(open(yfile,"rb"),delimiter=",")
 
     ytrain =  y[0:tr_size]
     ytest = y[tr_size:(tr_size+te_size)]
     
-    algorithms = ['l2r_lr','l2r_l2loss_svc_dual','l2r_l2loss_svc','l2r_l1loss_svc_dual','mcsvm_cs','l1r_l2loss_svc','l1r_lr','l2r_lr_dual']
+    algorithms = ['l1r_l2loss_svc','l1r_lr']
     for algorithm in algorithms:
-        for i in range(1,1000):
-            beta =1
-            w={0:0.1, 1:(1+i*.01)}
-            solver = mlpy.LibLinear(solver_type=algorithm, C=beta, eps=0.01, weight=w)
-            
-            solver.learn(xtrain, ytrain)         
+	print algorithm + " starting"
+        for i in range(1,10):
+	    for b in range(1,5):
+            	beta = .2*b
+            	w={0:1, 1:(.1+i*.1)}
+            	solver = mlpy.LibLinear(solver_type=algorithm, C=beta, eps=0.01, weight=w)
+            	solver.learn(xtrain, ytrain)         
 
-            yhat = solver.pred(xtrain)
-            printStats(ytrain,yhat,algorithm,w,beta,"train errors")
+            	yhat = solver.pred(xtrain)
+            #	printStats(ytrain,yhat,algorithm,w,beta,"train errors")
         
-            yhat = solver.pred(xtest)
-            printStats(ytest,yhat,algorithm,w,beta,"test errors")
+            	yhat = solver.pred(xtest)
+            	printStats(ytest,yhat,algorithm,w,beta,"test errors")
+    solver = mlpy.KNN(2)
+
+    solver.learn(xtrain, ytrain)         
+
+    yhat = solver.pred(xtrain)
+  #  printStats(ytrain,yhat,"Kmeans","none","none","train errors")
+    yhat = solver.pred(xtest)
+    printStats(ytest,yhat,"Kmeans","none","none","test errors")
+
+    
+    solver = mlpy.ClassTree()
+    solver.learn(xtrain, ytrain)         
+    yhat = solver.pred(xtrain)
+  #  printStats(ytrain,yhat,"Classification Tree","none","none","train errors")
+    yhat = solver.pred(xtest)
+    printStats(ytest,yhat,"Classification Tree","none","none","test errors")
+   
+    mlpy.KFDAC(lmb=0.001, kernel=mlpy.KernelGaussian(sigma=1.0))
+    solver.learn(xtrain, ytrain)         
+    yhat = solver.pred(xtrain)
+  #  printStats(ytrain,yhat,"KFDAC","none","none","train errors")
+    yhat = solver.pred(xtest)
+    printStats(ytest,yhat,"KFDAC","none","none","test errors")
+    
 
 if __name__ == "__main__":
     xfile = sys.argv[1]
